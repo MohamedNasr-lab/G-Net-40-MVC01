8df1c2a191c6f3e4d763871e89e5ff6e645c1a3d
MohamedNasr <mohamednasr27122@gmail.com>
2026-05-12 22:47:15 +0300

    Add project files.

---
+++ GymSystem/Configurations/PlanConfigurations.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GymSystem.Configurations
+{
+    public class PlanConfigurations : IEntityTypeConfiguration<Plan>
+    {
+        public void Configure(EntityTypeBuilder<Plan> builder)
+        {
+            builder.Property(p => p.Name).HasColumnType("varchar").HasMaxLength(50);
+
+            builder.Property(p => p.Description).HasMaxLength(200);
+
+            builder.Property(p => p.Price).HasPrecision(10, 2);
+
+            builder.Property(p => p.CreatedAt)
+       .HasDefaultValueSql("GETDATE()");
+
+            builder.ToTable(tb =>
+            {
+                tb.HasCheckConstraint("DurationCheckValue", "Duration Between 1 and 365");
+
+
+
+            });
+        }
+    }
+}
